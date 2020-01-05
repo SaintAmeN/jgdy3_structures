01b8a6ffa5dd13bfe5e7a13a0b4f4c8b25242fb7
Paweł Recław <pawel.reclaw@gmail.com>
2020-01-05 14:26:28 +0100

    first commit

---
+++ src/main/java/com/javagdy3/struktury/Zad1.java
@@ -0,0 +1,10 @@
+package com.javagdy3.struktury;
+
+import java.util.Stack;
+
+public class Zad1 {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+    }
+}
